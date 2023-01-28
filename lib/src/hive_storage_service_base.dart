e3b8236b5573ad1f63d46f7e372d9d2c2a3f267c
Grayson Erhard <develop@graysonerhard.com>
2023-01-27 17:23:23 -0700

    restored compaction functionality, but added a parameter for it.

--- lib/src/hive_storage_service_base.dart
+++ lib/src/hive_storage_service_base.dart
@@ -6,25 +6,28 @@ import 'package:path_provider/path_provider.dart';
 class HiveStorageService {
   final Function adapterRegistrationCallback;
   static const String subDirectory = 'hive';
+  final CompactionStrategy compactionStrategy;
 
-  HiveStorageService({required this.adapterRegistrationCallback});
+  HiveStorageService(
+      {required this.adapterRegistrationCallback,
+      required this.compactionStrategy});
 
   Future<void> init() async {
     await Hive.initFlutter(subDirectory);
+
     adapterRegistrationCallback();
   }
 
   /// Get a value from the cache.
   Future<T?> get<T>(String key, {T? defaultValue}) async {
-    final box = await Hive.openBox(key);
+    final box = await Hive.openBox(key, compactionStrategy: compactionStrategy);
     final data = await box.get(key, defaultValue: defaultValue) as T?;
     return data;
   }
 
   /// Create or update a cache entry.
   Future<void> set(String key, dynamic value) async {
-    final box = await Hive.openBox(key);
-    await box.compact();
+    final box = await Hive.openBox(key, compactionStrategy: compactionStrategy);
     box.put(key, value);
   }
 
